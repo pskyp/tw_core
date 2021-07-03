1d5349c35b2a71d053eaa390cbb4fdfe2721eb7e
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2021-07-03 16:54:44 +0500

    fixed compelte address algo
    Signed-off-by: ShahzadUmarBaig <ShahzadUBaig@gmail.com>

--- lib/models/location/location_model.dart
+++ lib/models/location/location_model.dart
@@ -50,13 +50,14 @@ class LocationModel {
   String get completeAddress {
     String result = '';
     for (int i = 0; i < formattedAddress.length; i++) {
-      if (formattedAddress[i].isNotEmpty) {
+      if (formattedAddress[i] != "") {
         result += formattedAddress[i];
-      }
-      if (i < formattedAddress.length - 1) {
-        result += ', ';
-      } else {
-        result += '.';
+
+        if (i < formattedAddress.length - 1) {
+          result += ', ';
+        } else {
+          result += '.';
+        }
       }
     }
     return result;
