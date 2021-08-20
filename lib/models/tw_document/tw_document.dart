230fa6eeb0dc3cc4433da5c3cc940a3dc77bc3fb
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2021-08-20 19:13:02 +0500

    updated documents model
    Signed-off-by: ShahzadUmarBaig <ShahzadUBaig@gmail.com>

--- lib/models/tw_document/tw_document.dart
+++ lib/models/tw_document/tw_document.dart
@@ -8,15 +8,16 @@ class TWDocument with EquatableMixin {
   final String docName;
   final String docPath;
   final String downloadURL;
+  final String instructions;
 
-  TWDocument({
-    required this.docName,
-    required this.docPath,
-    required this.downloadURL,
-  });
+  TWDocument(
+      {required this.docName,
+      required this.docPath,
+      required this.downloadURL,
+      required this.instructions});
 
   @override
-  List<Object?> get props => [docName, docPath, downloadURL];
+  List<Object?> get props => [docName, docPath, downloadURL, instructions];
   Map<String, dynamic> toJson() => _$TWDocumentToJson(this);
   factory TWDocument.fromJson(Map<String, dynamic> json) =>
       _$TWDocumentFromJson(json);
