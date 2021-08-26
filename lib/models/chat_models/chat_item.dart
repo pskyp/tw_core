58a13a634fdf6d4db7985eafbe081c4ec5fbaf7f
osama <sosamaalirizvi@gmail.com>
2021-08-26 16:44:27 +0500

    --wip-- [skip ci]

--- lib/models/chat_models/chat_item.dart
+++ lib/models/chat_models/chat_item.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:tw_core/theme/tw_theme.dart';
 
 part 'chat_item.g.dart';
 
@@ -39,3 +41,83 @@ class ChatItem extends Equatable {
         seenByAll,
       ];
 }
+
+//rawText in presetChat corresponds to text in ChatItem
+class PresetChat extends Equatable {
+  final String rawText;
+  final String shortText;
+  final String text;
+
+  PresetChat({
+    required this.rawText,
+    required this.shortText,
+    required this.text,
+  });
+
+  Widget get ui => Container(
+        margin: EdgeInsets.only(top: 8, left: 15, right: 15),
+        padding: EdgeInsets.all(13),
+        decoration: BoxDecoration(
+          color: TWTheme.kMainThemeColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+
+  @override
+  List<Object?> get props => [
+        rawText,
+        shortText,
+        text,
+      ];
+}
+
+List<PresetChat> allPresetChats = [
+  negotiationPresetChat,
+  offerPresetChat,
+  offerAcceptPresetChat,
+  offerDeclinePresetChat,
+  jobCompletePresetChat,
+];
+
+PresetChat? getPresetChat(String rawText) {
+  int index = allPresetChats.indexWhere(
+    (presetChat) => presetChat.rawText == rawText,
+  );
+  print("index of preset chat: ${index}");
+  return index == -1 ? null : allPresetChats[index];
+}
+
+PresetChat negotiationPresetChat = PresetChat(
+  rawText: '###SuperSecretNegotiationMessage###',
+  shortText: "The contractor is interested.",
+  text: '''Hi there! The contractor has shown interest
+       in your application for this job''',
+);
+
+PresetChat offerPresetChat = PresetChat(
+  rawText: '###SuperSecretOfferMessage###',
+  shortText: 'You were offered the job.',
+  text: '''You have been offered the job''',
+);
+
+PresetChat offerAcceptPresetChat = PresetChat(
+  rawText: '###SuperSecretOfferAcceptMessage###',
+  shortText: 'The job was accepted.',
+  text: '''The job was accepted''',
+);
+
+PresetChat offerDeclinePresetChat = PresetChat(
+  rawText: '###SuperSecretOfferDeclineMessage###',
+  shortText: 'The job offer was declined.',
+  text: '''The job offer was declined.''',
+);
+
+PresetChat jobCompletePresetChat = PresetChat(
+  rawText: '###SuperSecretJobCompleteMessage###',
+  shortText: 'The job was completed.',
+  text: '''The job was completed.''',
+);
