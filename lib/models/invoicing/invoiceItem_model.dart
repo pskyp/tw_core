50261c398bd8b6e332552a52e9d15d37d20a5c57
osama <sosamaalirizvi@gmail.com>
2021-09-10 15:34:53 +0500

    --wip-- [skip ci]

--- lib/models/invoicing/invoiceItem_model.dart
+++ lib/models/invoicing/invoiceItem_model.dart
@@ -1,30 +1,27 @@
-import 'dart:ui';
-
-import 'package:flutter/foundation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'invoiceItem_model.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class InvoiceItem {
-  String itemID;
+  final String itemName;
   final String descritpion;
-  final DateTime date;
+  // final DateTime date;
   final double qty;
   final double unitPrice;
   final double taxRate;
-  final double total;
-  String recieptImageURL;
+  // final double total;
+  // String recieptImageURL;
 
   InvoiceItem({
-    required this.itemID,
+    required this.itemName,
     required this.descritpion,
-    required this.date,
+    // required this.date,
     required this.qty,
     required this.unitPrice,
     required this.taxRate,
-    required this.total,
-    required this.recieptImageURL,
+    // required this.total,
+    // required this.recieptImageURL,
   });
   Map<String, dynamic> toJson() => _$InvoiceItemToJson(this);
   factory InvoiceItem.fromJson(Map<String, dynamic> json) =>
--- lib/models/invoicing/invoiceItem_model.g.dart
+++ lib/models/invoicing/invoiceItem_model.g.dart
@@ -7,24 +7,18 @@ part of 'invoiceItem_model.dart';
 // **************************************************************************
 
 InvoiceItem _$InvoiceItemFromJson(Map<String, dynamic> json) => InvoiceItem(
-      itemID: json['itemID'] as String,
+      itemName: json['itemName'] as String,
       descritpion: json['descritpion'] as String,
-      date: DateTime.parse(json['date'] as String),
       qty: (json['qty'] as num).toDouble(),
       unitPrice: (json['unitPrice'] as num).toDouble(),
       taxRate: (json['taxRate'] as num).toDouble(),
-      total: (json['total'] as num).toDouble(),
-      recieptImageURL: json['recieptImageURL'] as String,
     );
 
 Map<String, dynamic> _$InvoiceItemToJson(InvoiceItem instance) =>
     <String, dynamic>{
-      'itemID': instance.itemID,
+      'itemName': instance.itemName,
       'descritpion': instance.descritpion,
-      'date': instance.date.toIso8601String(),
       'qty': instance.qty,
       'unitPrice': instance.unitPrice,
       'taxRate': instance.taxRate,
-      'total': instance.total,
-      'recieptImageURL': instance.recieptImageURL,
     };
