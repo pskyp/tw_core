f5db0006aeff27807851aaf3b8e31902bb9ac4ef
Piers Wilcox <pierswilcox@gmail.com>
2021-10-29 22:08:40 +0100

    updated auth bloc

--- lib/infrastructure/auth/firebase_auth_facade.dart
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -4,11 +4,42 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tw_core/models/auth/auth_failure.dart';
 import 'package:tw_core/models/auth/i_auth_facade.dart';
 import 'package:tw_core/models/email/email.dart';
+import 'package:tw_core/models/password/password.dart';
+import 'package:crypto/crypto.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'dart:convert';
+
+
 
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth firebaseAuth;
   final GoogleSignIn googleSignIn;
   FirebaseAuthFacade({required this.firebaseAuth, required this.googleSignIn});
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress email,
+    required Password password,
+  }) async {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(
+        email: email.getOrCrash(),
+        password: password.getOrCrash(),
+      );
+      return right(unit);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-disabled') return left(AuthFailure.userDisabled());
+      if (e.code == 'wrong-password')
+        return left(AuthFailure.wrongEmailOrPassword());
+      if (e.code == 'user-not-found') return left(AuthFailure.userNotFound());
+      if (e.code == 'user-disabled') return left(AuthFailure.userDisabled());
+      if (e.code == 'wrong-password')
+        return left(AuthFailure.wrongEmailOrPassword());
+      if (e.code == 'user-not-found') return left(AuthFailure.userNotFound());
+      if (e.code == 'invalid-email') return left(AuthFailure.invalidEmail());
+      return left(AuthFailure.serverError());
+    }
+  }
 
   @override
   Future<Either<AuthFailure, Unit>> sendSignInLinkToEmail({
@@ -81,6 +112,38 @@ class FirebaseAuthFacade implements IAuthFacade {
     //   print('in side catch failure');
     //   return left(const AuthFailure.serverError());
     // }
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  Future<Either<AuthFailure, Unit>> signInWithApple() async {
+    try {
+      final rawNonce = generateNonce();
+      final nonce = sha256ofString(rawNonce);
+
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        nonce: nonce,
+      );
+
+      final oauthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        rawNonce: rawNonce,
+      );
+      var authResult;
+      await firebaseAuth.signInWithCredential(oauthCredential);
+      return right(unit);
+    } catch (e) {
+      print("failed");
+      return left(AuthFailure.serverError());
+    }
   }
 
   @override
--- lib/models/auth/auth_failure.dart
+++ lib/models/auth/auth_failure.dart
@@ -10,4 +10,7 @@ abstract class AuthFailure with _$AuthFailure {
   const factory AuthFailure.invalidEmail() = InvalidEmail;
   const factory AuthFailure.emaillAlreadyInUse() = EmaillAlreadyInUse;
   const factory AuthFailure.invalidCredentials() = InvalidCredentials;
+  const factory AuthFailure.userNotFound() = UserNotFound;
+  const factory AuthFailure.userDisabled() = UserDisabled;
+  const factory AuthFailure.wrongEmailOrPassword() = WrongEmailOrPassword;
 }
